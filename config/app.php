34f5fb6183ddaa8e456b05cc16aa6d6b830626e9
Arrilot <nekrasov@greensight.ru>
2020-11-17 16:37:49 +0300

    clean configs from aws and pusher

--- config/app.php
+++ config/app.php
@@ -172,7 +172,6 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
--- config/services.php
+++ config/services.php
@@ -24,10 +24,4 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
 ];
