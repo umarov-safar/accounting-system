7305afdf3d5fdea3d386d3fc70a0d8f8d3915704
Arrilot <nekrasov@greensight.ru>
2020-11-18 14:43:29 +0300

    namespace in api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (Request $request) {
-    return [ "a" => "b"];
+Route::namespace('V1')->prefix('v1')->group(function () {
+    Route::get('/test', function (Request $request) {
+        return [ "a" => "b"];
+    });
 });
