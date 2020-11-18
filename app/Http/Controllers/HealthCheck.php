30d7e98feb0e99e1e748e3af65ddee1e2006a75b
Arrilot <nekrasov@greensight.ru>
2020-11-18 14:36:34 +0300

    Move healthcheck to class

---
+++ app/Http/Controllers/HealthCheck.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HealthCheck extends Controller
+{
+    public function __invoke()
+    {
+        return "OK";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\HealthCheck;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('health', function (Request $request) {
-    return 'OK';
-});
+Route::get('health', HealthCheck::class);
 
