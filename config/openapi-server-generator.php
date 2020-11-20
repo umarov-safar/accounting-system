de9123e7d69458f5db9151cf58c69803063449a5
Arrilot <nekrasov@greensight.ru>
2020-11-21 00:18:43 +0300

    install greensight/laravel-openapi-server-generator

---
+++ config/openapi-server-generator.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /**
+     * Path to the directory where index.yaml openapi file located
+     */
+    'apidoc_dir' => public_path('api-docs'),
+
+    /**
+     * Path to the directory where dto model files are generated
+     * Matches the -o option in openapi generator
+     */
+    'output_dir' => base_path('generated'),
+
+    /**
+     * Path relative to the app directory where dto models will be located
+     */
+    'app_dir' => 'OpenApiGenerated',
+];
