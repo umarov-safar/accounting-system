11da4f111c3e8bf08cb25502b31f1c43fbd67184
arrilot <nekrasov@greensight.ru>
2021-05-28 16:33:54 +0300

    #76419

--- config/pagination.php
+++ config/pagination.php
@@ -2,4 +2,14 @@
 
 return [
     'default_limit' => 10,
+
+    /*
+    |----------------------------------------------------
+    | Default pagination type if it's not set in request.
+    |----------------------------------------------------
+    |
+    | Available types: "offset", "cursor".
+    |
+    */
+    'default_type' => 'offset',
 ];
