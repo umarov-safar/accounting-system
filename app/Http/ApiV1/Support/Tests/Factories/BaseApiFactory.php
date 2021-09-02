4ae772900cd9d4cd01f75e2ce347a0a1ee0dcdf7
Ilya Nekrasov <nekrasov.ilya90@gmail.com>
2021-09-02 12:21:44 +0000

    #79892

---
+++ app/Http/ApiV1/Support/Tests/Factories/BaseApiFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\ApiV1\Support\Tests\Factories;
+
+use Greensight\TestFactories\Factory;
+
+abstract class BaseApiFactory extends Factory
+{
+    public ?int $id = null;
+
+    public function withId(?int $id = null): static
+    {
+        return $this->immutableSet('id', $id ?? $this->faker->randomNumber());
+    }
+
+    protected function optionalId()
+    {
+        return $this->whenNotNull($this->id, $this->id);
+    }
+}
---
+++ tests/IntegrationTestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class IntegrationTestCase extends TestCase
+{
+    use DatabaseTransactions;
+}
