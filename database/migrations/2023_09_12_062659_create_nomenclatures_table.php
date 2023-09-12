6494bacd36276fb26368b21432d45d198f314837
Safar Umarov <safarumarov711@gmail.com>
2023-09-12 13:37:54 +0700

    #496 Разработать структуру и модель номенклатуры

---
+++ database/migrations/2023_09_12_062659_create_nomenclatures_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nomenclatures', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_service');
+            $table->string('obj_type');
+            $table->unsignedBigInteger('obj_id');
+            $table->bigInteger('base_price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nomenclatures');
+    }
+};
