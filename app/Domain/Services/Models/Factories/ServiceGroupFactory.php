04e6d3b36b1e29a5293ac594214d4746787ca8db
Safar Umarov <safarumarov711@gmail.com>
2023-09-12 17:48:48 +0700

    Structure of model #500

---
+++ app/Domain/Services/Models/Factories/ServiceGroupFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domain\Services\Models\Factories;
+
+use App\Domain\Services\Models\ServiceGroup;
+use Ensi\LaravelTestFactories\BaseModelFactory;
+
+class ServiceGroupFactory extends BaseModelFactory
+{
+    protected $model = ServiceGroup::class;
+
+    public function definition()
+    {
+
+    }
+}
