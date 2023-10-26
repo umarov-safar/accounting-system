8e203a5f18a12cb2cdafbd6a4326fd7e4706d906
Safar Umarov <safarumarov711@gmail.com>
2023-10-26 14:25:07 +0700

    Zippy client was added #574

--- app/Domain/Nomenclatures/Actions/CreateNomenclatureAction.php
+++ app/Domain/Nomenclatures/Actions/CreateNomenclatureAction.php
@@ -3,12 +3,26 @@
 namespace App\Domain\Nomenclatures\Actions;
 
 use App\Domain\Nomenclatures\Models\Nomenclature;
+use Eijen\Zstore\Api\NomenclaturesApi;
+use Eijen\Zstore\Model\CreateNomenclatureRequest;
 use Illuminate\Support\Arr;
 
 class CreateNomenclatureAction
 {
+    public function __construct(
+        protected  NomenclaturesApi $nomenclaturesApi
+    )
+    {
+    }
+
     public function execute(array $fields)
     {
+        $request = new CreateNomenclatureRequest(['itemname' => $fields['seller_id']]);
+
+
+        $response = $this->nomenclaturesApi->createNomenclature($request);
+        $fields['zippy_nomenclature_id'] = $response->getData()->getItemId();
+
         return Nomenclature::create(Arr::only($fields, Nomenclature::FILLLABLE));
     }
 }
